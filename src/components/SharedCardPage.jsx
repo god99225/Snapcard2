912a4e5c6e5b17060cbf91f1a2bd7260792025af
Prem Borkar <premborkar9922573922@gmail.com>
2025-04-27 10:17:48 +0530

    update6

--- src/components/SharedCardPage.jsx
+++ src/components/SharedCardPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import '../style/SharedCard.css'; // Make sure modal CSS is there
+import '../style/ViewCard.css';
 
 function SharedCardPage() {
   const query = new URLSearchParams(useLocation().search);
